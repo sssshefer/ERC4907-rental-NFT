91b2f006f3390dec80ac50632cc307fafe68c793
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-08-07 02:42:05 +0300

    Small bug fixes

--- contracts/ERC4907.sol
+++ contracts/ERC4907.sol
@@ -6,8 +6,8 @@ import "./IERC4907.sol";
 
 contract ERC4907 is ERC721, IERC4907 {
     struct UserInfo {
-        address user, 
-        uint65 expires
+        address user;
+        uint64 expires;
     }
 
     mapping(uint => UserInfo) public users;
@@ -16,7 +16,7 @@ contract ERC4907 is ERC721, IERC4907 {
 
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
         return interfaceId == type(IERC4907).interfaceId ||
             super.supportsInterface(interfaceId);
     }
@@ -24,15 +24,15 @@ contract ERC4907 is ERC721, IERC4907 {
     function setUser(uint tokenId, address user, uint64 expires) public virtual {
         require(_isApprovedOrOwner(msg.sender, tokenId));
 
-        UserInfo userInfo storage = users[tokenId];
+        UserInfo storage userInfo  = users[tokenId];
         userInfo.user = user;
         userInfo.expires = expires;
 
         emit UpdateUser(tokenId, user, expires);
     }
 
-    function userOf(uint tokenId) public view returns (address) virtual{
-        return(block.timestamp > expires ? address(0): users[tokenId].user);
+    function userOf(uint tokenId) public view virtual returns (address){
+        return(block.timestamp > users[tokenId].expires ? address(0): users[tokenId].user);
     }
 
     function userExpires(uint tokenId) external view returns (uint){
@@ -47,7 +47,7 @@ contract ERC4907 is ERC721, IERC4907 {
     ) internal virtual override{
         super._beforeTokenTransfer(from,to, tokenId, batchSize);
         
-        if(from != to && _users[tokenId].user != address(0)){
+        if(from != to && users[tokenId].user != address(0)){
             delete users[tokenId];
 
             emit UpdateUser(tokenId, address(0), 0);
--- contracts/ERC4907Demo.sol
+++ contracts/ERC4907Demo.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import "./ERC4907.sol";
 
 contract ERC4907Demo is ERC4907 {
-    constructor(string _name, string _symbol) ERC4907(_name, _symbol) {}
+    constructor(string memory _name, string memory _symbol) ERC4907(_name, _symbol) {}
 
     function mint(uint tokenId, address to) public {
         _mint(to, tokenId);
