052e1dabc99bb21571114e5b0f4310345c014169
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-08-07 02:48:36 +0300

    tests for ERC4907

--- test/ERC4907.test.ts
+++ test/ERC4907.test.ts
@@ -1,19 +1,46 @@
 import { loadFixture, ethers, expect } from "./setup";
-//import type { ERC4907 } from "../typechain-types";
+import type { ERC4907Demo } from "../typechain-types";
 
-describe("ERC4907", function () {
+describe("ERC4907 tests", function () {
   async function deploy() {
-    const [user1, user2] = await ethers.getSigners();
+    const [owner, addr1, addr2] = await ethers.getSigners();
 
-    const Factory = await ethers.getContractFactory("ERC4907", user1);
-    const erc4907 = await Factory.deploy();
-    await erc4907.waitForDeployment();
+    const Factory = await ethers.getContractFactory("ERC4907Demo", owner);
+    const erc4907demo:ERC4907Demo = await Factory.deploy("ShefToken", "SHEF");
+    await erc4907demo.waitForDeployment();
 
-    return { user1, user2, erc4907 }
+    const tokenId = 1;
+    await erc4907demo.mint(tokenId, owner.address);
+
+    return { erc4907demo, owner, addr1, addr2, tokenId }
   }
 
-  it("Deploy test", async function () {
-    const { user1, user2, erc4907 } = await loadFixture(deploy);
+  it("Should set user and get user", async function () {
+    const { erc4907demo, owner, addr1, tokenId } = await loadFixture(deploy);
+    const expires = Math.floor(Date.now() / 1000) + 3600; // 1 hour from now
 
+    await erc4907demo.connect(owner).setUser(tokenId, addr1.address, expires);
+
+    expect(await erc4907demo.userOf(tokenId)).to.equal(addr1.address);
+  });
+
+  it("Should return address(0) after user expires", async function () {
+    const { erc4907demo, owner, addr1, tokenId } = await loadFixture(deploy);
+    const expires = Math.floor(Date.now() / 1000) + 1; // 1 second from now
+
+    await erc4907demo.connect(owner).setUser(tokenId, addr1.address, expires);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    expect(await erc4907demo.userOf(tokenId)).to.equal(ethers.ZeroAddress);
+  });
+
+  it("Should delete user on token transfer", async function () {
+    const { erc4907demo, owner, addr1, addr2, tokenId } = await loadFixture(deploy);
+    const expires = Math.floor(Date.now() / 1000) + 3600; // 1 hour from now
+
+    await erc4907demo.connect(owner).setUser(tokenId, addr1.address, expires);
+    await erc4907demo.connect(owner).transferFrom(owner.address, addr2.address, tokenId);
+
+    expect(await erc4907demo.userOf(tokenId)).to.equal(ethers.ZeroAddress);
   });
 })
